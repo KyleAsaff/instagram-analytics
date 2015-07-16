401e8b6dbef602c5f1be2ff5722a0c5fa6cdb63f
KyleAsaff <kyleasaff@gmail.com>
2015-07-16 14:50:24 -0700

    Takes parameters

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,12 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Test."))
+  }
+  
+  def hashtags(start: Integer, end: Integer) = Action {
+    // url es = http://localhost:9000/
+    Ok("Start: " +start+" End: "+end)
   }
 
 }
